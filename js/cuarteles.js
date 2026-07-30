62be2d78c108ca9349ccf9e5861c43073925541e
AnntoBarbero <antyy_96@hotmail.com>
2026-07-30 17:47:33 -0300

    Corrige la extensión de la imagen de Trenel en el archivo cuarteles.js

--- js/cuarteles.js
+++ js/cuarteles.js
@@ -443,7 +443,7 @@ const cuarteles = [
         correos:{
             oficial:"asoc.trenel@bomberosargentina.org.ar"
         },
-        imagen:"../img/cuarteles/trenel.png",
+        imagen:"../img/cuarteles/trenel.PNG",
         galeria:[],
         redes:{
             facebook:"",
